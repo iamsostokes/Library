26b13b2f2850dfeec99a0e2bfda0da835ae8c245
Jamez Stokes <jamezstokes@MacBook-Pro-6.local>
2019-01-24 14:51:13 -0500

    I completed making all of my getters and setters.

--- src/com/company/Book.java
+++ src/com/company/Book.java
@@ -13,7 +13,35 @@ public class Book {
        this.completedReading = completedReading;
     }
 
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
+    public String getGenre() {
+        return genre;
+    }
 
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public boolean isCompletedReading() {
+        return completedReading;
+    }
+
+    public void setCompletedReading(boolean completedReading) {
+        this.completedReading = completedReading;
+    }
 }

0b0780d875799069ec892c99d9d13b18011d6b5a
Jamez Stokes <jamezstokes@MacBook-Pro-6.local>
2019-01-24 14:39:20 -0500

    commiting my start project file.  the only thing that I really did is create my variables for my class.

---
+++ src/com/company/Book.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Book {
+    private String title;
+    private String genre;
+    private int numberOfPages;
+    private boolean completedReading;
+
+    public Book (String title, String genre, int numberOfPages, boolean completedReading) {
+       this.title = title;
+       this.genre = genre;
+       this.numberOfPages = numberOfPages;
+       this.completedReading = completedReading;
+    }
+
+
+
+
+}
